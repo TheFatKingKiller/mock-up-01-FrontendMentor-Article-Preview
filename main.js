9bea9a72a9931f601e36876c626c3f05e7d5fa6d
Arash Soltani <soltaniworld@gmail.com>
2020-08-20 18:59:46 -0400

    reorganized share-buttons. made desktop/mobile css separate. added js to hide contact info and img for mobile only.

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-const shareBubble = document.querySelector(".share-box.flex-container");
+const shareBubble = document.querySelector(".share-grid");
 const shareBtn = document.querySelector(".person-share");
 
+let width = document.documentElement.clientWidth;
+
 shareBtn.addEventListener("click", ()=>{
-    shareBubble.classList.toggle("hidden");
+    shareBubble.classList.toggle("show-share");
+    if(width < 375){
+        document.querySelector(".person img").classList.toggle("hidden");
+        document.querySelector(".person-text").classList.toggle("hidden");
+    }
+    else{
+        document.querySelector(".person img").classList.remove("hidden");
+        document.querySelector(".person-text").classList.remove("hidden");  
+    }
+})
+
+window.addEventListener('resize', ()=>{
+    width = document.documentElement.clientWidth;
 })
